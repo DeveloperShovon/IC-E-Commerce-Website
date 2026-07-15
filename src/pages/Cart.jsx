a15cd3ffb7cf5f7356544d9d17589f4ea2169fef
ShovonDas-Dev <shovon.das@genexinfosys.com>
2026-07-15 20:17:26 +0600

    build problem

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { clearCart } from "../features/cartSlice";
-import CartItem from "../component/Cart/CartItem.jsx";
+import CartItem from "../component/Cart/Cartitem.jsx";
 
 
 
